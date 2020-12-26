f712fe2f2d94f456a17d759b9445f424c7c29ab6
Yilehan <76608180+Yilehan@users.noreply.github.com>
2020-12-26 14:44:56 +0800

    Add files via upload

---
+++ 背单词/背单词/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace 背单词
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.tsCbSelect = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.tsCbSpeed = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStripLabel2 = new System.Windows.Forms.ToolStripLabel();
+            this.tsCbDisplay = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.ForeColor = System.Drawing.Color.Maroon;
+            this.label1.Location = new System.Drawing.Point(69, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 46);
+            this.label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label2.Location = new System.Drawing.Point(76, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 21);
+            this.label2.TabIndex = 1;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton1.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.toolStripButton1.ForeColor = System.Drawing.Color.Blue;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(72, 22);
+            this.toolStripButton1.Text = "选择词汇：";
+            // 
+            // tsCbSelect
+            // 
+            this.tsCbSelect.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.tsCbSelect.Items.AddRange(new object[] {
+            "四级",
+            "六级",
+            "考研"});
+            this.tsCbSelect.Name = "tsCbSelect";
+            this.tsCbSelect.Size = new System.Drawing.Size(75, 25);
+            this.tsCbSelect.SelectedIndexChanged += new System.EventHandler(this.tsCbSelect_SelectedIndexChanged);
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.toolStripLabel1.ForeColor = System.Drawing.Color.Blue;
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(68, 22);
+            this.toolStripLabel1.Text = "刷新速度：";
+            // 
+            // tsCbSpeed
+            // 
+            this.tsCbSpeed.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.tsCbSpeed.Items.AddRange(new object[] {
+            "1000",
+            "2000",
+            "3000",
+            "4000",
+            "5000"});
+            this.tsCbSpeed.Name = "tsCbSpeed";
+            this.tsCbSpeed.Size = new System.Drawing.Size(75, 25);
+            this.tsCbSpeed.TextChanged += new System.EventHandler(this.tsCbSpeed_TextChanged);
+            // 
+            // toolStripLabel2
+            // 
+            this.toolStripLabel2.ForeColor = System.Drawing.Color.DarkRed;
+            this.toolStripLabel2.Name = "toolStripLabel2";
+            this.toolStripLabel2.Size = new System.Drawing.Size(40, 22);
+            this.toolStripLabel2.Text = "(毫秒)";
+            // 
+            // tsCbDisplay
+            // 
+            this.tsCbDisplay.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.tsCbDisplay.Items.AddRange(new object[] {
+            "正序显示",
+            "倒序显示",
+            "随机显示"});
+            this.tsCbDisplay.Name = "tsCbDisplay";
+            this.tsCbDisplay.Size = new System.Drawing.Size(75, 25);
+            this.tsCbDisplay.SelectedIndexChanged += new System.EventHandler(this.tsCbDisplay_SelectedIndexChanged);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.tsCbSelect,
+            this.toolStripLabel1,
+            this.tsCbSpeed,
+            this.toolStripLabel2,
+            this.tsCbDisplay});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.toolStrip1.Size = new System.Drawing.Size(462, 25);
+            this.toolStrip1.Stretch = true;
+            this.toolStrip1.TabIndex = 3;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 200;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(462, 156);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "背单词";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripComboBox tsCbSelect;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripComboBox tsCbSpeed;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel2;
+        private System.Windows.Forms.ToolStripComboBox tsCbDisplay;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ 背单词/背单词/Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 背单词
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int idx = 0;//索引值
+
+        public int displayMode = 0;//显示模式（正序、倒序、随机）
+
+        public string[] english;//英文部分
+
+        public string[] chinese;//释义部分
+
+        public string filePath; //词汇文件路径
+        
+        public SortedDictionary<string, string> dict = new SortedDictionary<string, string>();//存储字典
+     
+        //窗体加载
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //默认显示
+            tsCbSelect.SelectedIndex = 0;
+            tsCbSpeed.SelectedIndex = 2;
+            tsCbDisplay.SelectedIndex = 0;
+
+            //加载词汇文件
+            filePath = @".\words\College_Grade4.txt";
+            ReadFile(filePath);
+
+            //设置显示方式/刷新速度   
+            this.TopMost = true;
+            displayMode = 1;
+            timer1.Interval = 3000;
+            timer1.Enabled = true;
+        }
+
+        //读取词汇文件
+        public void ReadFile(string path)
+        {
+            StreamReader sw = new StreamReader(path, Encoding.Default);
+
+            string content = sw.ReadToEnd();//读取全部数据
+            string[] lines = content.Split('\n');//按行拆分
+            string[] words = new string[2];
+            english = new string[lines.Length];
+            chinese = new string[lines.Length];
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                lines[i] = lines[i].Trim();
+                int index = lines[i].IndexOf(' ');//将每一行按空格拆分成英文部分和释义部分
+                if (index < 0) continue;
+                
+                if (index > 0 && index < lines[i].Length)
+                {
+                    words[0] = lines[i].Substring(0, index).Trim();
+                    words[1] = lines[i].Substring(index).Trim();
+                }
+                if (!dict.ContainsKey(words[0]))
+                {
+                    dict.Add(words[0], words[1]); 
+                }
+                english[i] = words[0];
+                chinese[i] = words[1];
+            }
+
+        }
+
+        //自动隐藏窗体
+        void AutoSideHideOrShow()
+        {
+            int sideThickness = 4;//边缘的厚度，窗体停靠在边缘隐藏后留出来的可见部分的厚度  
+
+            //如果窗体最小化或最大化了则什么也不做  
+            if (this.WindowState == FormWindowState.Minimized || this.WindowState == FormWindowState.Maximized)
+            {
+                return;
+            }
+
+            //如果鼠标在窗体内  
+            if (Cursor.Position.X >= this.Left && Cursor.Position.X < this.Right && Cursor.Position.Y >= this.Top && Cursor.Position.Y < this.Bottom)
+            {
+                //如果窗体离屏幕边缘很近，则自动停靠在该边缘  
+                if (this.Top <= sideThickness)
+                {
+                    this.Top = 0;
+                }
+                if (this.Left <= sideThickness)
+                {
+                    this.Left = 0;
+                }
+                if (this.Left >= Screen.PrimaryScreen.WorkingArea.Width - this.Width - sideThickness)
+                {
+                    this.Left = Screen.PrimaryScreen.WorkingArea.Width - this.Width;
+                }
+            }
+            //当鼠标离开窗体以后  
+            else
+            {
+                //隐藏到屏幕左边缘  
+                if (this.Left == 0)
+                {
+                    this.Left = sideThickness - this.Width;
+                }
+                //隐藏到屏幕右边缘  
+                else if (this.Left == Screen.PrimaryScreen.WorkingArea.Width - this.Width)
+                {
+                    this.Left = Screen.PrimaryScreen.WorkingArea.Width - sideThickness;
+                }
+                //隐藏到屏幕上边缘  
+                else if (this.Top == 0 && this.Left > 0 && this.Left < Screen.PrimaryScreen.WorkingArea.Width - this.Width)
+                {
+                    this.Top = sideThickness - this.Height;
+                }
+            }
+        }
+
+        //定时显示单词
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (displayMode == 1)//正序显示
+            {
+                
+                label1.Text = english[idx];
+                label2.Text = chinese[idx];
+
+                idx++;
+                if (idx >= english.Length) idx = 0;
+
+            }
+            else if (displayMode == 2)//倒序显示
+            {
+                
+                label1.Text = english[idx];
+                label2.Text = chinese[idx];
+
+                idx--;
+                if (idx <= 0) idx = english.Length - 1;
+            }
+            else if (displayMode == 3)//乱序显示
+            {
+                Random rnd = new Random();
+                idx = rnd.Next(english.Length);
+                label1.Text = english[idx];
+                label2.Text = chinese[idx];
+            }
+
+            
+
+
+        }
+
+        //选择词汇文件
+        private void tsCbSelect_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (tsCbSelect.Text)
+            {
+                case "四级":
+                    filePath = @".\words\College_Grade4.txt";
+
+                    break;
+                case "六级":
+                    filePath = @".\words\College_Grade6.txt";
+                    break;
+                case "考研":
+                    filePath = @".\words\brace_brutalbrace.txt";
+                    break;
+            }
+
+            idx = 0;
+            ReadFile(filePath);
+
+        }
+
+        //设置刷新速度
+        private void tsCbSpeed_TextChanged(object sender, EventArgs e)
+        {
+            int time = 0;
+            bool flg = int.TryParse(tsCbSpeed.Text.Trim(),out time);
+            if (flg && time >0)
+            {
+                timer1.Interval = time;
+                //timer1.Enabled = true;
+            }
+            else
+            {
+                MessageBox.Show("输入格式错误：请输入一个数字！");
+            }
+             
+        }
+        //设置显示方式
+        private void tsCbDisplay_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (tsCbDisplay.Text)
+            {
+                case "正序显示":
+                    displayMode = 1;
+                    idx = 0;
+                    break;
+                case "倒序显示":
+                    displayMode = 2;
+                    idx = english.Length-1;
+                    break;
+                case "随机显示":
+                    displayMode = 3;
+                    break;
+            }
+        }
+
+        //自动停靠到桌面边缘
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            timer2.Interval = 200;
+            AutoSideHideOrShow();
+        }
+    }
+}
